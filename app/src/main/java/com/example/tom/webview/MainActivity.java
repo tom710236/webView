e784d8cd9e64124276ddd11042729680fb486fa0
TOM <tom710236@gmail.com>
2017-02-16 18:08:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tom/webview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tom.webview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView mWebView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_hi_ticket);
+        mWebView = (WebView)findViewById(R.id.webView);
+        mWebView.setWebViewClient(mWebViewClient);
+        mWebView.setInitialScale(1);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://demo.shinda.com.tw/dist/");
+    }
+    WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    };
+}
